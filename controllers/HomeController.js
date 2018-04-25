b53b696bb96cef88bee2b8adcdfd20b8a1ed97d6
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-04-25 12:30:38 +0530

    team flag

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,10 +1,52 @@
+
+var request 		= require('request');
+var cheerio 		= require('cheerio');
+var http = require('http'),
+Stream = require('stream').Transform;
 var fs 				= require('fs');
 var HomeController = {
 
+	index  : function(req, res){
+		var url = "http://www.cricbuzz.com/cricket-team/women";
+		var urlArr = [
+			"http://www.cricbuzz.com/cricket-team",
+			"http://www.cricbuzz.com/cricket-team/domestic",
+			"http://www.cricbuzz.com/cricket-team/league",
+			"http://www.cricbuzz.com/cricket-team/women"
+		];
+		for(let u of urlArr) {
+			let url = u;
+			request(url, function(error, response, html){
+				
+				var $ = cheerio.load(html);
+				var result = [];
+				$(".cb-team-item").each(function(i,e){
+					var teamLink = $(e).find('a').attr('href');
+					var teamImg =  $(e).find('img').attr('src');
+					var teamname = $(e).find('a').attr('title');
+					teamname = teamname.split(' ').join('_').toLowerCase();
+					http.request("http:"+teamImg, function(responseImg) {                                        
+					  var data = new Stream();                                                    
 
-	index  : function(request, response){
+					  responseImg.on('data', function(chunk) {                                       
+					    data.push(chunk);                                                         
+					  });                                                                         
+
+					  responseImg.on('end', function() {                                             
+					    fs.writeFileSync("public/files/team-flags/"+teamname+".png", data.read());                               
+					  });                                                                         
+					}).end();
+					result.push({
+						name : $(e).find('a').attr('title'),
+						teamLink : teamLink,
+						teamImg : teamImg
+					})
+				})
+				res.send(result);
+			})
+		}
 		//response.render("index");
-		response.send("index");
+		//response.send("index");
 	},
 
 }
--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,8 @@
 
 var router = global.express.Router();
 
-
-var home = require("../controllers/DataController");
+var homeController = require("../controllers/HomeController");
+var dataController = require("../controllers/DataController");
 global.app.use(function(req, response, next) {
    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -10,13 +10,14 @@ global.app.use(function(req, response, next) {
     response.setHeader("Access-Control-Allow-Headers", "access-control-allow-credentials,access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,content-type");
   return next();
 });
-router.get("/", home.index);
-router.get("/match/:id", home.details);
-router.get("/match-score/:id", home.renderMatchScore);
-router.get("/series", home.renderSeries);
-router.get("/series-details/:id/:slug", home.renderSerieDetails);
-router.get("/ranking/:type", home.randerRanking)
-router.get("/ranking/:type/:matchType/:position", home.randerRanking)
-router.get("/news", home.renderNews);
+router.get("/", dataController.index);
+router.get("/match/:id", dataController.details);
+router.get("/match-score/:id", dataController.renderMatchScore);
+router.get("/series", dataController.renderSeries);
+router.get("/series-details/:id/:slug", dataController.renderSerieDetails);
+router.get("/ranking/:type", dataController.randerRanking)
+router.get("/ranking/:type/:matchType/:position", dataController.randerRanking)
+router.get("/news", dataController.renderNews);
 
+router.get('/team-flags',homeController.index);
 module.exports = router;
